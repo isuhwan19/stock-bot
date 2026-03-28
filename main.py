285dcce5a66c5d275afefd4f2c6d3abb3005ab82
isuhwan19 <isuhwan19@gmail.com>
2026-03-28 13:48:42 +0900

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import datetime
+from datetime import timezone, timedelta
+import us_market
+import surge_stock
+
 def main():
     KST = timezone(timedelta(hours=9))
     now = datetime.datetime.now(KST)
@@ -6,3 +11,7 @@ def main():
     print("테스트 모드: 모든 기능을 실행합니다.")
     us_market.run()
     surge_stock.run()
+
+# 👇 파이썬에게 "이 파일이 열리면 main() 함수를 실행해!"라고 명령하는 핵심 코드입니다.
+if __name__ == "__main__":
+    main()
